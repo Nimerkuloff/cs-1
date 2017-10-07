4d012dff2e62add8a000a66bb6b2d3d7a1cb7a5f
Nimerkuloff <Nimerkuloff@users.noreply.github.com>
2017-10-07 17:17:48 +0700

    Solve 1 out of  2

--- Labrabota3/Labrabota3/Labrabota3.cpp
+++ Labrabota3/Labrabota3/Labrabota3.cpp
@@ -1,35 +1,41 @@
 // Labrabota3.cpp : Defines the entry point for the console application.
-//
 
 #include "stdafx.h"
 #include <stdio.h>
 #include <stdlib.h>
-
+#pragma hdrstop
 int main()
 {
 	const int A = 10;
-	int array[A] = {}, box, counter = 0, counterMax = 0, counterMaxMeta = 0, j = 0;
-	//Input
-	for (int i = 0; i < A; i++) {
-		scanf("%i", &array[i]);
-	}
-	//2
+	//int array[A] = {1,0,1,1,1,0,1,1,1,-9}, box, counter = 0, counterMax = 1, counterMaxMeta = 0, j = 0;
+  int array[A] = {}, box, counter = 1, counterMax = 0, counterMaxMeta = 0, j = 0;
+	
+for (int i = 0; i < A; i++) {
+	scanf("%i", &array[i]);
+}
+	box = array[0] + 1;
 	for (j = 0; j < A;j++) {
-	box = array[j];
-	if (counterMax > counterMaxMeta) {
-		counterMaxMeta = counterMax;
-	}
-	for (int i = 0; i < A; i++) {
-			if (array[i] == box) {
-				counter++;
+		if (array[j] != box) {
+			box = array[j];
+			counter = 0;
+			if (counterMax >= counterMaxMeta) {
+				counterMaxMeta = counterMax;
+				counterMax = 0;
 			}
-			else if (counter > counterMax) {
-				counterMax = counter;
-				counter = 1;
+			for (int i = 0; i < A; i++) {
+				if (array[i] == box) {
+					counter++;
+				}
+				else if (counter >= counterMax) {
+					counterMax = counter;
+					counter = 0;
+				}
 			}
 		}
 	}
-	//2
+
+	printf("Answer is %i\n",counterMaxMeta);
+	system("PAUSE");
     return 0;
 }
 
