fc3873b8eb1649178348ad26e9980dbe4ac0be32
Nick Merkulov <he.has.not.email@gmail.com>
2017-10-09 03:39:19 +0700

    Completed Labrabota2-2 properly

---
+++ Labrabota 2/Labrabota2-2/Labrabota22.cpp
@@ -0,0 +1,29 @@
+// Labrabota22.cpp : Defines the entry point for the console application.
+//
+#include "stdafx.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#pragma hdrstop
+int main(void)
+{
+	int number, sum = 0;
+	printf("Type a number\n");
+	scanf("%3i",&number);
+	int backup = number;
+	if ((number > 99) && (number < 1000)) {
+		while (number != 0) {
+			sum += number % 10;
+			number /= 10;
+		}
+
+		if ((pow(backup, 2) == (pow(sum,3)))) {
+			printf("Are equivalent");
+		}
+		system("PAUSE");
+	}
+	else {
+		printf("Try with a valid number next time...");
+	}
+	return 0;
+}
