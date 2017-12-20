1e9eb19d5fd102f9fdc808345ca202da5381b3f1
Nimerkuloff <Nimerkuloff@users.noreply.github.com>
2017-12-21 00:57:53 +0700

    shitted some 3-2

--- Labrabota3/Labrabota3-1.c
+++ Labrabota3/Labrabota3-1.c
@@ -1,26 +1,30 @@
-
-
-//Program finds max length of a repeated numbers sequences (elements of array input manually) 
-//#include "stdafx.h"
+﻿//Ищу длиннейшую последовательность одинаковых элементов массива
+#include "stdafx.h"
 #include <stdio.h>
 #include <stdlib.h>
-#pragma hdrstop
+//#pragma hdrstop
+#include <iostream>
+using namespace std;
 int main()
 {
+	printf("A[%i]=\n", max);
 	int counter = 1, max = 0;
 	const int A = 10;
-	int array[A] = {};
-	scanf("%u", &array[0]);
-	for (int i = 1; i <= A; i++) {
-		scanf("%u", &array[i]);
-		if (array[i - 1] == array[i]) {
+	int array[A];
+	cin >> array[0];
+	for (int i = 1; i < A; i++) {
+		cin >> array[i];
+
+		if (array[i] == array[i - 1]) {
 			counter++;
-			if (counter >= max) { max = counter; }
-			else { counter = 1; }
+		}
+		else { counter = 1; }
+
+		if (counter >= max) {
+			max = counter;
 		}
 
 	}
 	printf("Answer is %i\n", max);
-	system("PAUSE");
 	return 0;
 }
--- Labrabota3/Labrabota3-2.c
+++ Labrabota3/Labrabota3-2.c
@@ -1,9 +1,82 @@
-// Labrabota32.cpp : Defines the entry point for the console application.
-//
-
+/*Функция переписывает строку.
+Если она находит в строке число, то вместо него она переписывает в выходную строку
+соответствующее по счету слово из входной строки.
+(например, "aaa bb1bb cc2cc" - "aaa bbaaabb ccbb1bbcc").
+Вывести результат на экран.
+*/
 #include "stdafx.h"
 #include <stdio.h>
+#include <stdlib.h>
+//#pragma hdrstop
+#include <iostream>
 
-int main(void) {
-	
+
+
+using namespace std;
+int countWords(char *str) {
+	char space = ' ';
+	int numOfSpaces = 0;
+	for (int i = 0; i <= strlen(str); i++) {
+		if (str[i] == space) {
+			numOfSpaces++;
+		}
+	}
+	if (numOfSpaces == 0) {
+		return 1;
+	}
+	else {
+		return numOfSpaces;
+	}
+}
+
+char* selectWordByNumber(int num, char *str) {
+	char *outStr;
+	outStr = (char *)malloc(200 * sizeof(char));
+	char space = ' ';
+	int tabCount = 0;
+	int i = 0;
+	if (num == 1) {
+		while (str[i] != space) {
+			strcpy(outStr, &str[i]);
+			i++;
+		}
+	}
+	else if (num>1) {
+		while (tabCount < (num - 1)) {
+			if (str[i] == space) {
+				tabCount++;
+			}
+			i++;
+		}
+		i++;
+		while (str[i] != space) {
+			strcpy(outStr, &str[i]);
+			i++;
+			outStr++;
+		}
+	}
+	outStr[i + 1] = '\0';
+	return outStr;
+}
+
+
+//char doTheJob(char *str) {}
+int main()
+{
+	char str;
+	char *pointerToString;
+	cout << "Dai stroku\n";
+	cin >> str;
+
+	pointerToString = &str;
+	//doTheJob(pointerToString);
+	char *out = selectWordByNumber(2, pointerToString);
+	int g = 0;
+	while (out[g] != '\0') {
+		cout << out[g];
+		g++;
+	}
+
+	return 0;
+
 }
