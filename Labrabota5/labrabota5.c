b129e2716a340d3f3611e5ea633c1138a794b904
Nimerkuloff <Nimerkuloff@users.noreply.github.com>
2017-12-21 22:05:26 +0700

    commit from visual studio

--- Labrabota5/labrabota5.c
+++ Labrabota5/labrabota5.c
@@ -10,12 +10,13 @@
 #include <windows.h>
 #include <iostream>
 #include <string.h>
+#include<limits>
 using namespace std;
-void func(int m, int n, int n1, int n2, int val) {
+void func(int rows, int cols, int n1, int n2, int val) {
 	int **ary;
-	const unsigned int DIM1 = m;
-	const unsigned int DIM2 = n;
-	// создание
+	const unsigned int DIM1 = rows;	//строки
+	const unsigned int DIM2 = cols;	//столбцы
+									// создание
 	ary = new int *[DIM1];    // массив указателей 
 	for (int i = 0; i < DIM1; i++) {
 		ary[i] = new int[DIM2];     // инициализация указателей
@@ -28,21 +29,23 @@ void func(int m, int n, int n1, int n2, int val) {
 		}
 	}
 	printf("Before\n");//вывод матрицы до преобразований 
-	for (int i = 0; i < m; i++)
-	{
-		for (int j = 0; j < n; j++)
+	for (int i = 0; i < rows; i++) {
+		for (int j = 0; j < cols; j++) {
 			cout << ary[i][j] << " ";
+		}
 		cout << endl;
 	}
+
 	// прибавление строк
 	for (int i = 0; i < DIM2; i++) {
-		ary[n2][i] = ary[n1][i] * val;
+		ary[n2][i] += ary[n1][i] * val;
 	}
 	printf("After\n");//вывод матрицы после преобразований 
-	for (int i = 0; i < m; i++)
-	{
-		for (int j = 0; j < n; j++)
+	for (int i = 0; i < rows; i++) {
+		for (int j = 0; j < cols; j++) {
+
 			cout << ary[i][j] << " ";
+		}
 		cout << endl;
 	}
 	return;
@@ -51,38 +54,49 @@ int main()
 {
 	int m, n, n1, n2, val;
 	printf("How many rows?\n");
-	do
-	{
-		printf("(integers only)");
-		cin >> m;
-	} while (isdigit(m) == 0);
+	while (!(cin >> m)) {
+		// Reset the input:
+		cin.clear();
+		// Get rid of the bad input before return was pressed:
+		while (cin.get() != '\n') {
+			continue;
+		}
+		// Ask user to try again:
+		cout << "Please enter a number:  ";
+	}
 
 	printf("How many columns?\n");
-	do
-	{
-		printf("(integers only)");
-		cin >> n;
-	} while (isdigit(n) == 0);
-
+	while (!(cin >> n)) {
+		// Reset the input:
+		cin.clear();
+		// Get rid of the bad input before return was pressed:
+		while (cin.get() != '\n') {
+			continue;
+		}
+		// Ask user to try again:
+		cout << "Please enter a number:  ";
+	}
 
 	printf("First row: ");
 	cin >> n1;
-	if ((n1 < 0) || (n1 > m)) {
+	if ((n1 < 0) || (n1 >= m)) {
 		printf("Bad number!\n");
 		do {
 			printf("Try again\n");
 			cin >> n1;
-		} while (((n1 < 0) || (n1 > m)) == 1);
+		} while (((n1 < 0) || (n1 >= m)) == 1);
 	}
+
 	printf("Second row: ");
 	cin >> n2;
-	if ((n2 < 0) || (n2 > m)) {
+	if ((n2 < 0) || (n2 >= m)) {
 		printf("Bad number!\n");
 		do {
 			printf("Try again\n");
 			cin >> n2;
-		} while (((n2 < 0) || (n2 > m)) == 1);
+		} while (((n2 < 0) || (n2 >= m)) == 1);
 	}
+
 	printf("Value:");
 	cin >> val;
 
